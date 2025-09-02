f4bd76653ab487eccd02bd8e987fc79bb1cc3da2
mdmd37597-prog <mdmd37597@gmail.com>
2025-09-02 12:58:58 +0100

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -5,8 +5,8 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
-const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
-const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+const PAGE_ACCESS_TOKEN = "EAAOGSi6yzuIBPYJdZCThgMsduUHiSM3OkErtbrZC5bK5stPlNh2u9ZCYaauZC6xuLflYvnZCH4ow070U29nTICZAbvVn2zaVvuRly7NZCr6kkCHAY1w2ZBSHLYVCsxSCm0DEQGFJEoZCurHX8fgEYKVcTpHhvXpi0Cb3L3ORDPJDIihvzE9YY6TZB0yyNHbAYTjrZB6io6tfJey";
+const VERIFY_TOKEN = "ABCD123;
 
 let userAnswers = {}; // تخزين الأجوبة الصحيحة
 
